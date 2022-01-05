3f27ccd076f05100bbabc8e76617e537157f6326
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-01-05 10:21:21 -0600

    added and built 'sign in' page

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -4,12 +4,14 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Header from './Components/Header_footer/Header';
 import Footer from './Components/Header_footer/Footer';
 import Home from './Components/Home';
+import SignIn from './Components/SignIn';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Header />
       <Switch>
+        <Route path="/sign_in" component={SignIn} />
         <Route path="/" exact component={Home} />
       </Switch>
       <Footer />
