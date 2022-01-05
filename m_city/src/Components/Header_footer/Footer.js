95c0adb379139097e00fefeb86d3d34306d1c427
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-01-05 07:00:17 -0600

    added logo component to header and footer

--- m_city/src/Components/Header_footer/Footer.js
+++ m_city/src/Components/Header_footer/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import { CityLogo } from '../Utils/tools';
+
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo link={true} linkTo={'/'} width="70px" height="70px" />
+      </div>
+      <div className="gooter_descl">
+        Manchest city 2022. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
